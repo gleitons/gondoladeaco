e3151823006f0a302df94fbb05570bfcb31161b3
gleitons <31367377+gleitons@users.noreply.github.com>
2022-11-01 06:37:42 -0300

    fasat

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,21 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+// import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+// import Link from 'next/link'
+
 
 
 export default function Home() {
   return (
-    <div>
-      
+    <>
+      <Head>
+        <title>Pagina Principal</title>
+        <meta name="description" content="Gôndola de Aço a preço de fábrica, gondoladeaco.com.br" />
+        <meta name="keywords" content="gôndola de aço, prateleira de aço, montar mercado, prateleiras com desconto" />
+       
+      </Head>
       <h1>Gôndola de Aço</h1>
       <h2>Orçamento gratuito</h2>
-      
-      
-    </div>
+    </>
   )
 }
--- pages/lp.jsx
+++ pages/lp.jsx
@@ -1,9 +1,20 @@
 import styles from '../styles/Lp.module.css';
-import '../styles/Home.module.css'
+import '../styles/Home.module.css';
+import Head from 'next/head';
 import Link from 'next/link'
+import Image from 'next/image';
+import gondola from '../public/img/gondolas-otimize-espaco.jpg'
+import gondola2 from '../public/img/gondolas-otimize-espaco-min-min-2-min.jpg'
 export default function LandPage() {
+
     return (
+
         <>
+            <Head>
+                <title>Landing Page - Gôndola de Aço</title>
+                <meta name="description" content="Gôndola de Aço a preço de fábrica, gondoladeaco.com.br" />
+                <meta name="keywords" content="gôndola de aço, prateleira de aço, montar mercado, prateleiras com desconto" />
+            </Head>
             <div className={styles.fundoForm}>
                 <div className={styles.Iform} >
                     <div className={styles.InfoB} >
@@ -18,7 +29,7 @@ export default function LandPage() {
                                     <input type="text" name="01_Nome" id="nome" />
                                     <input type="email" name="02_Email" id="email" />
                                 </div>
-                               
+
                                 <select name="Selecione" id="selecione">
                                     <option value="gondola">Gôndolas</option>
                                     <option value="prateleira">Prateleira</option>
@@ -34,9 +45,41 @@ export default function LandPage() {
                     </div>
                 </div>
             </div>
+            {/* <div className={styles.imgHeader}>
+                <Link href="https://brasilgondolas.com.br" target="_blank"><img src="./img/gondolas-otimize-espaco-min-min.jpg" alt="" /></Link>
+            </div> */}
+
             <div className={styles.imgHeader}>
-            <Link href="https://brasilgondolas.com.br" target="_blank"><img src="./img/gondolas-otimize-espaco-min-min.jpg" alt="" /></Link>
+                <Link href="https://brasilgondolas.com.br" target="_blank"> <Image
+                src={gondola2}
+                // width={100}
+                // height={300}
+                // object-fit = 'fill'
+                // display = 'block'
+                // object-position = 'relative'
+                alt="Fazer Orçamento de Gôndolas"
+            /></Link>
             </div>
+            {/* <Image
+                src={gondola2}
+                // width={100}
+                // height={300}
+                object-fit = 'fill'
+                display = 'block'
+                object-position = 'relative'
+                alt="Fazer Orçamento de Gôndolas"
+            /> */}
+            {/* <div>
+                <Image
+                    alt=""
+                    src={gondola}
+                    // width={500} 
+                    // height={500} 
+                    priority
+                    blurDataURL="data:..." 
+                    placeholder="blur" // Optional blur-up while loading
+                />
+            </div> */}
         </>
     )
 }
